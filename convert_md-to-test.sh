da049411bb39267c6ecc01b20b6a8f9424488c90
Sophie Eckenstaler <sophie.eckenstaler@outlook.com>
2022-06-14 12:39:00 +0200

    added css

--- convert_md-to-test.sh
+++ convert_md-to-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# change into the script directory
+current_dir=$(dirname "${BASH_SOURCE[0]}")
+cd $current_dir && pwd
 # path to input directory
 input_dir="./_input"
 #  path to output directory
@@ -6,6 +9,8 @@ output_dir="./_output"
 #  path to csl
 csl_dir="./furesh-templates/csl"
 csl="$csl_dir/chicago-author-date_slides.csl"
+#  path to css
+css_dir="./furesh-templates/css"
 # path to template directory
 templates_dir="./furesh-templates"
 # variables
@@ -21,5 +26,5 @@ for file in $input_dir/*.md;
 		docker run --rm \
        	--volume "$(pwd):/data" \
        	--user $(id -u):$(id -g) \
-       	pandoc/core:2.18 -s -f markdown -t $output_format --citeproc --csl $csl --reference-doc $templates_dir/$template $file -o $output_dir/$name-$output_name
+       	pandoc/core:2.18 -s -f markdown -t $output_format --citeproc --csl $csl --include-in-header $css_dir/slidy-furesh.html --template $templates_dir/$template $file -o $output_dir/$name-$output_name
 done
