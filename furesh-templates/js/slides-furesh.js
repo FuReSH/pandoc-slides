2a6176517be82af0eef2e04114717f79dc57319b
Sophie Eckenstaler <sophie.eckenstaler.1@hu-berlin.de>
2022-11-29 17:58:52 +0100

    added currentDate-fct

--- furesh-templates/js/slides-furesh.js
+++
@@ -1,7 +0,0 @@
-function setCurrentDate(element) {
-    date = new Date().toLocaleDateString();
-    //element.setAttribute("content", date);
-    //document.write(date);
-    element.innerText = date;
-    console.log(element);
-}
